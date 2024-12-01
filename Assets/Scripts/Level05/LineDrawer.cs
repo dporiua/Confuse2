66864528332309048be03bac296f19109b92fb31
Dana <tetekim31@gmail.com>
2024-12-01 11:11:51 +0400

    box logic complete

--- Assets/Scripts/Level05/LineDrawer.cs
+++ Assets/Scripts/Level05/LineDrawer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,9 @@ namespace Dana
     /// <summary>
     /// This script is meant to draw a line based on colliders.
     /// It uses LineRenderer to render line points and deletes the line
-    /// if it collides with a maze wall or the player releases the left mouse button.
+    /// Changes material colour after a short delay as well.
     /// </summary>
-
+   
     public class LineDrawer : MonoBehaviour
     {
         #region Variables
@@ -23,9 +24,16 @@ namespace Dana
         [Tooltip("Assign a layer related to maze wall into this slot, this will help the script identify places where the pen can't go beyond.")]
         [SerializeField] private LayerMask wallLayerMask;
 
+        [Header("Line Settings")]
+        [Tooltip("Delay time before the line is cleared.")]
+        [SerializeField] private float clearDelay = 0.6f;
+        [Tooltip("Assign a colour from the colour wheel to represent line error colour")]
+        [SerializeField] private Color incorrectLineCOlour = Color.red;
+
         private LineRenderer _currentLine;
         private List<Vector3> _linePoints = new List<Vector3>();
         private bool _isDrawing = false;
+        private bool _isClearing = false; 
 
         #endregion
 
@@ -40,19 +48,22 @@ namespace Dana
         /// </summary>
         private void HandleDrawingInput()
         {
-            if (Input.GetMouseButtonDown(0))
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            bool isGroundHit = Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundLayerMask);
+
+            if (Input.GetMouseButtonDown(0) && isGroundHit)
             {
                 StartDrawing();
             }
 
-            if (_isDrawing && Input.GetMouseButton(0))
+            if (_isDrawing && Input.GetMouseButton(0) && isGroundHit)
             {
-                AddPoint();
+                AddPoint(hit.point);
             }
 
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0) && !_isClearing)
             {
-                ClearCurrentLine();
+                StartCoroutine(ClearLineWithDelay());
             }
         }
 
@@ -62,6 +73,7 @@ namespace Dana
         private void StartDrawing()
         {
             _isDrawing = true;
+            _isClearing = false;
 
             GameObject line = Instantiate(lineRendererPrefab, transform.position, transform.rotation);
             _currentLine = line.GetComponent<LineRenderer>();
@@ -76,72 +88,50 @@ namespace Dana
         /// <summary>
         /// Adds points to the line while the player is drawing.
         /// </summary>
-        private void AddPoint()
+        private void AddPoint(Vector3 worldPosition)
         {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            if (_linePoints.Count == 0 || Vector3.Distance(worldPosition, _linePoints[_linePoints.Count - 1]) > 0.1f)
             {
-                if (IsGround(hit.collider))
+                if (CheckWallCollision(worldPosition) && !_isClearing)
                 {
-                    Vector3 worldPosition = hit.point;
-
-                    if (_linePoints.Count == 0 || Vector3.Distance(worldPosition, _linePoints[_linePoints.Count - 1]) > 0.1f)
-                    {
-                        if (CheckWallCollision(worldPosition))
-                        {
-                            ClearCurrentLine();
-                            return;
-                        }
-
-                        _linePoints.Add(worldPosition);
-                        _currentLine.positionCount = _linePoints.Count;
-                        _currentLine.SetPositions(_linePoints.ToArray());
-                    }
+                    StartCoroutine(ClearLineWithDelay());
+                    return;
                 }
-                else if (IsWall(hit.collider))
-                {
-                    ClearCurrentLine();
-                }
+
+                _linePoints.Add(worldPosition);
+                _currentLine.positionCount = _linePoints.Count;
+                _currentLine.SetPositions(_linePoints.ToArray());
             }
         }
 
         /// <summary>
-        /// Clears current line when leaving mouse button or hitting a wall.
+        /// Clears the line after a delay while changing its color to signify line deleting feedback.
         /// </summary>
-        private void ClearCurrentLine()
+        private IEnumerator ClearLineWithDelay()
         {
+            _isClearing = true;
+
             if (_currentLine != null)
             {
+                _currentLine.material.color = incorrectLineCOlour;
+
+                yield return new WaitForSeconds(clearDelay);
+
                 Destroy(_currentLine.gameObject);
+                _linePoints.Clear();
+                _isDrawing = false;
+                _isClearing = false;
             }
-            _linePoints.Clear();
-            _isDrawing = false;
         }
 
-        #region Bools.
+        #region Bools
         /// <summary>
-        /// Checks if drawing's postion overlaps with any wall colliders.
+        /// Checks if drawing's position overlaps with any wall colliders.
         /// </summary>
         private bool CheckWallCollision(Vector3 position)
         {
             Collider[] colliders = Physics.OverlapSphere(position, 0.05f, wallLayerMask);
             return colliders.Length > 0;
-        }
-
-        /// <summary>
-        /// Checks if a collider belongs to the ground layer.
-        /// </summary>
-        private bool IsGround(Collider collider)
-        {
-            return collider.gameObject.layer == LayerMask.NameToLayer("MazeGround");
-        }
-
-        /// <summary>
-        /// Checks if a collider belongs to the wall layer.
-        /// </summary>
-        private bool IsWall(Collider collider)
-        {
-            return collider.gameObject.layer == LayerMask.NameToLayer("MazeWall");
         }
         #endregion
         #endregion
---
+++ Assets/Scripts/Level05/ValuableBoxRotation.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dana
+{
+    /// <summary>
+    /// Rotates the object this script is attached to around using the
+    /// left mouse button.
+    /// </summary>
+
+    public class ValuableBoxRotation : MonoBehaviour
+    {
+        #region Variables
+        [Tooltip("Change the speed of object rotation by changing the value. The higher the number, the faster the rotation.")]
+        [SerializeField] private float rotationSpeed = 50f; 
+
+        private bool isDragging = false;
+        private Vector3 lastMousePosition;
+        #endregion
+
+        private void Update()
+        {
+            HandleMouseInput();
+        }
+
+        #region Private Functions.
+        private void HandleMouseInput()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    if (hit.collider.gameObject == gameObject) 
+                    {
+                        isDragging = true;
+                        lastMousePosition = Input.mousePosition;
+                    }
+                }
+            }
+
+            if (isDragging && Input.GetMouseButton(0))
+            {
+                RotateObject();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+            }
+        }
+
+        private void RotateObject()
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+            Vector3 deltaMousePosition = currentMousePosition - lastMousePosition;
+
+            float rotationX = -deltaMousePosition.y * rotationSpeed * Time.deltaTime;
+            float rotationY = deltaMousePosition.x * rotationSpeed * Time.deltaTime;
+
+            transform.Rotate(rotationX, rotationY, 0, Space.World); 
+
+            lastMousePosition = currentMousePosition; 
+        }
+        #endregion
+    }
+}
